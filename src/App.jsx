b5729b0824483764ca4c781b20620f79a723356b
Ulia <ulianaescha05@gmail.com>
2025-03-23 16:59:25 +0200

    Change files name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/main-page/MainPage";
-import Registration from "./pages/auth/Registration";
+import Registration from "./pages/registration/Registration";
 
 function App() {
   return (
